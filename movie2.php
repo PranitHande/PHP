8fdc0447db0bd0f219d68f15ba3fb8ad68d7ea60
Hello123 <78649671+PranitHande@users.noreply.github.com>
2022-05-23 18:00:41 +0530

    SETB2chmovie.php

--- movie2.php
+++ movie2.php
@@ -1,12 +1,13 @@
+#Download SetB2movie1.php first and then this file :)
 <?php
                 $doc=simplexml_load_file("movie.xml");
 		echo "Movie info";
                 foreach($doc->category as $movie)
                 {
                                 echo "<br>"."Category : ".$movie->category."<br>";
-				echo "<br>"."Book no : ".$movie->mno."<br>";
-				echo "<br>"."Book name : ".$movie->moviename."<br>";
-				echo "<br>"."Author name : ".$movie->actorname."<br>";	
+				echo "<br>"."Movie no : ".$movie->mno."<br>";
+				echo "<br>"."Movie name : ".$movie->moviename."<br>";
+				echo "<br>"."Actor name : ".$movie->actorname."<br>";	
 				echo "<br>"."Year : ".$movie->year."<br>";		
                 }
                 
